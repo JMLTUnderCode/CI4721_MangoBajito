057860e0edafd84811144186f03edbcba7f498f6
A6575 <astridaalvaradoa@gmail.com>
2025-02-21 22:16:00 -0400

    Agregado definciones y funcionalidades para la tabla de simbolos. Falta probar

---
+++ Etapa2/src/mango-bajito.cpp
@@ -0,0 +1,74 @@
+#include "mango-bajito.hpp"
+
+// Implementacion de la clase SymbolTable
+SymbolTable::SymbolTable() {
+	this->current_scope = 0;
+	this->next_scope = 1;
+	this->scopes.push({this->current_scope, false});
+}
+
+void SymbolTable::open_scope() {
+	this->current_scope = this->next_scope;
+	this->next_scope++;
+	this->scopes.push({this->current_scope, false});
+}
+
+void SymbolTable::close_scope() {
+	this->scopes.pop();
+	this->current_scope = this->scopes.top().first;
+}
+
+bool SymbolTable::insert_symbol(string symbol_name, Attributes &attr) {
+	if (contains_key(this->table, symbol_name)) {
+		vector<Attributes> &symbols = this->table[symbol_name];
+		for (Attributes &symbol : symbols) {
+			if (symbol.scope == this->current_scope) {
+				return false; // El simbolo ya existe en el scope actual
+			}
+		}
+		symbols.push_back(attr);
+		return true;
+	}
+	vector<Attributes> symbols;
+	symbols.push_back(attr);
+	this->table[symbol_name] = symbols;
+	return true;
+}
+
+Attributes* SymbolTable::search_symbol(string symbol_name) {
+	Attributes *predef_symbol, *best_option = nullptr;
+	stack<pair<int, bool> > scopes_aux = this->scopes;
+	int scope_value;
+
+	auto symbols = this->table.find(symbol_name);
+	if (symbols != this->table.end()) {
+		for (Attributes &symbol : symbols->second) {
+			if (symbol.symbol_name != symbol_name) continue;
+			if (symbol.scope == 0) { predef_symbol = &symbol; break; }
+			
+			while (!scopes_aux.empty()) {
+				scope_value = scopes_aux.top().first;
+				
+				if (symbol.scope == scope_value) {
+					best_option = &symbol;		// El scope mas cercano
+					break;
+				} else if (best_option != nullptr && scope_value == best_option->scope) {
+					break;						// No se encontrara un scope mas cercano
+				}
+
+				if (scopes_aux.top().second) break;	  // No puede ver al padre
+				
+				scopes_aux.pop();
+			}
+		}
+
+		if (best_option != nullptr){
+			return best_option;
+		}else if (predef_symbol != nullptr){
+			return predef_symbol;
+		}else{
+			return nullptr;
+		}
+	}
+
+}
---
+++ Etapa2/src/mango-bajito.hpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <variant>
+
+using namespace std;
+
+struct RecursiveArray; // Declaración adelantada
+
+// Definicion de tipo Information para almacenar la informacion de los atributos
+typedef variant<string, int, bool> Information;
+// Definicion de tipo Values para almacenar el valor de los atributos
+typedef variant<
+	nullptr_t,
+	int, 
+	bool, 
+	float, 
+	double, 
+	string,
+	int*,                    // Puntero a int
+	double*,                 // Puntero a double
+	string*,                 // Puntero a string
+	vector<int>, 
+	vector<float>, 
+	vector<double>, 
+	vector<std::string>,
+	vector<RecursiveArray>   // Recursión para matrices anidadas
+> Values;
+
+// Estructura para representar arreglos anidados recursivos
+struct RecursiveArray {
+    vector<Values> data; // Puede contener cualquier tipo de Values
+};
+
+// Definicion de Category para almancenar las categorias de los simbolos
+enum Category {
+	CONSTANT,
+	VARIABLE,
+	FUNCTION,
+	PARAMETERS,
+	ARRAY,
+	STRUCT,
+	UNION,
+	FIELD,
+	TYPE,
+};
+
+// Implementacion de la estructura de Atributos
+struct Attributes {
+	string symbol_name;
+	Category category;
+	int scope;
+	SymbolTable *type;
+	vector<pair<Information, Attributes*> > info = {{"", nullptr}}; // Informacion de los atributos (depende de Category)
+	Values value = nullptr;
+};
+
+// funcion auxiliar para verificar si una key esta en un map
+bool contains_key(unordered_map<string, vector<Attributes>> &table, string key) {
+	return table.count(key) > 0;
+}
+
+// Implementacion Tabla de Simbolos Le-Blanc Cook
+class SymbolTable {
+	protected:
+		unordered_map<string, vector<Attributes> > table;	//Tabla de simbolos
+		stack<pair<int, bool> > scopes;				//Pila de scopes
+		int current_scope;							//Scope actual
+		int next_scope;								//Proximo scope
+	public:
+		SymbolTable();
+		~SymbolTable() = default;
+		void open_scope();											//Abrir nuevo scope
+		void close_scope();											//Cerrar scope 
+		bool insert_symbol(string symbol_name, Attributes &attr);	//Insertar simbolo en la tabla
+		Attributes* search_symbol(string symbol_name);				//Buscar simbolo en la tabla
+};
