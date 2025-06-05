60d8691d74da59015818cf40846c72be8fd30d9d
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-06-05 14:01:39 -0400

    Actualizacion de numeros de tests en la actualizacion

--- MangoBajito/1-Lexer_Parser_SymTable_AST/update_test.sh
+++ MangoBajito/1-Lexer_Parser_SymTable_AST/update_test.sh
@@ -2,7 +2,7 @@
 
 # Define las secciones y los números de test a actualizar para valid
 declare -A valid_tests
-valid_tests["Declaracion"]="00 01 02 03 04 05 06 07 08 09 10 11"
+valid_tests["Declaracion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13"
 valid_tests["Funcion"]="00 01 02"
 valid_tests["Operacion"]="05 06 07 08 09"
 
