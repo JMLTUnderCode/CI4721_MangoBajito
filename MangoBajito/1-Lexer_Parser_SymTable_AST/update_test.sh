551b53f106a8f0e7f87a2a9de978723caf5b9d45
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-06-11 22:31:28 -0400

    Inclusion de prueba de negacion

--- MangoBajito/1-Lexer_Parser_SymTable_AST/update_test.sh
+++ MangoBajito/1-Lexer_Parser_SymTable_AST/update_test.sh
@@ -6,7 +6,7 @@ valid_tests["Bucle"]="00 02"
 valid_tests["Condicional"]="00 01 02"
 valid_tests["Declaracion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13"
 valid_tests["Funcion"]="00 01 02"
-valid_tests["Operacion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19"
+valid_tests["Operacion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
 valid_tests["ManejoError"]=""
 
 # Define las secciones y los números de test a actualizar para errors
@@ -15,7 +15,7 @@ error_tests["Bucle"]="00"
 error_tests["Condicional"]="00"
 error_tests["Declaracion"]=""
 error_tests["Funcion"]="00 01 02"
-error_tests["Operacion"]="16 17 18 19"
+error_tests["Operacion"]="16 17 18 19 20"
 error_tests["ManejoError"]=""
 
 # Ruta base de los tests
