5a464b6da662bcb6c769cd74731823d90125f3bd
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-06-13 16:58:00 -0400

    Inclusion de archivos de errors y valid para bucles a nivel de rango

--- MangoBajito/lenguajes2_uptodate_tests.sh
+++ MangoBajito/lenguajes2_uptodate_tests.sh
@@ -3,7 +3,7 @@
 # Define las secciones y los números de test a actualizar para valid
 declare -A valid_tests
 valid_tests["Apuntador"]=""
-valid_tests["Bucle"]="00 02"
+valid_tests["Bucle"]="00 01 03"
 valid_tests["Casteo"]="00 01 02 03 04"
 valid_tests["Condicional"]="00 01 02"
 valid_tests["Declaracion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13"
@@ -14,7 +14,7 @@ valid_tests["ManejoError"]=""
 # Define las secciones y los números de test a actualizar para errors
 declare -A error_tests
 error_tests["Apuntador"]=""
-error_tests["Bucle"]="00"
+error_tests["Bucle"]="00 01 02"
 error_tests["Casteo"]="00 01 02 03"
 error_tests["Condicional"]="00"
 error_tests["Declaracion"]=""
