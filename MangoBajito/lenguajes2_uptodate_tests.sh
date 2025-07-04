bd5e311297f6b2e31d53e5329577b3fe87f95aac
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-07-04 15:31:31 -0400

    Actualizacion de test para condicion guardias logicas

--- MangoBajito/lenguajes2_uptodate_tests.sh
+++ MangoBajito/lenguajes2_uptodate_tests.sh
@@ -5,7 +5,7 @@ declare -A valid_tests
 valid_tests["Apuntador"]=""
 valid_tests["Bucle"]="00 01 02 03 04"
 valid_tests["Casteo"]="00 01 02 03 04"
-valid_tests["Condicional"]="00 01 02"
+valid_tests["Condicional"]="00 01 02 03"
 valid_tests["Declaracion"]="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14"
 valid_tests["Entrada_Salida"]="00 01"
 valid_tests["Funcion"]="00 01 02"
